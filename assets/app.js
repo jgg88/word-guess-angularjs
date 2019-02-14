b0985466d75fb4f08441a0f94608a45d23be63ce
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2019-02-14 17:39:10 -0500

    logic for all displayed char complete

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@ app.controller('ctrl', ($scope) => {
     $scope.inputGuess = '';
     $scope.displayed = '';
     let wordArray = ['Game of Thrones', 'Rick and Morty', 'The Office', 'Westworld', 'King of Queens', 'The IT crowd'];
-    let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)].toLowerCase();
+    let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)].toUpperCase();
 
     $scope.generateWord = () => {
         console.log(randomWord)
@@ -23,13 +23,22 @@ app.controller('ctrl', ($scope) => {
     $scope.generateWord();
 
     $scope.guessedLetter = () => {
+        let correctLetter = false;
         for (letter in randomWord) {
-            if (randomWord[letter] === $scope.inputGuess) {
-                $scope.correctGuesses.push($scope.inputGuess);
+            if (randomWord[letter] === $scope.inputGuess.toUpperCase()) {
+                correctLetter = true;
                 let displayedArr = $scope.displayed.split('');
-                displayedArr.splice(letter, 1, $scope.inputGuess);
+                displayedArr.splice(letter, 1, $scope.inputGuess.toUpperCase());
                 $scope.displayed = displayedArr.join('');
             }
+        }
+        if (!correctLetter) {
+            $scope.guessesRemaining--;
+            $scope.incorrectGuesses.push($scope.inputGuess.toUpperCase());
+        }
+        if (correctLetter) {
+            $scope.correctGuesses.push($scope.inputGuess.toUpperCase());
+            correctLetter = false;
         }
         $scope.inputGuess = '';
     }
