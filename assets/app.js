6a4d76d890f50ad29d663fd25d67fae90adb9988
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2019-02-13 21:42:50 -0500

    prog on logic

---
+++ assets/app.js
@@ -0,0 +1,35 @@
+let app = angular.module('wordGame', []);
+
+app.controller('ctrl', ($scope) => {
+
+    $scope.guessesRemaining = 5;
+    $scope.incorrectGuesses = [];
+    $scope.correctGuesses = [];
+    $scope.inputGuess = '';
+    $scope.displayed = '';
+    let wordArray = ['Game of Thrones', 'Rick and Morty', 'The Office', 'Westworld', 'King of Queens', 'The IT crowd'];
+    let randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+
+    $scope.generateWord = () => {
+        console.log(randomWord)
+        for (i=0; i < randomWord.length; i++) {
+            if (randomWord[i] === ' ') {
+                $scope.displayed += ' ';
+            } else {
+                $scope.displayed += '-';
+            }
+        }
+    }
+    $scope.generateWord();
+
+    $scope.guessedLetter = () => {
+        for (letter in randomWord) {
+            if (randomWord[letter] === $scope.inputGuess) {
+                $scope.correctGuesses.push($scope.inputGuess);
+                let displayedArr = $scope.displayed.split('');
+                displayedArr.splice(randomWord[letter], 1, $scope.inputGuess);
+                $scope.displayed = displayedArr.join('');
+            }
+        }
+    }
+});
